edfa833df27c819ebf7f8d66d1cd7ed662e58d81
Montajur Rahman Monir <montajurrahmanmonir@gmail.com>
2025-05-18 04:58:06 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ var mixer = mixitup(containerEl, {
     effects: "fade translateZ(-100px)",
   },
 });
+
+
+
+$('.sliders').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+ prevArrow: '<i class="fa-solid fa-circle-chevron-left prev"></i>',
+ nextArrow: '<i class="fa-solid fa-circle-chevron-right next"></i>',
+ dots:true,
+});
+
+
+
+// {/*  */}
